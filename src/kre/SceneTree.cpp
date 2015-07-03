582260e58c24c6fb35787656255588cfc57aa4a4
Kristina Simpson <sweet.kristas@gmail.com>
2015-07-04 10:27:44 +1200

    First draft of scene tree class.

--- src/kre/SceneTree.cpp
+++ src/kre/SceneTree.cpp
@@ -21,8 +21,202 @@
 	   distribution.
 */
 
+#include "CameraObject.hpp"
+#include "BlendModeScope.hpp"
+#include "ClipScope.hpp"
+#include "ColorScope.hpp"
+#include "DisplayDevice.hpp"
+#include "LightObject.hpp"
+#include "Renderable.hpp"
+#include "RenderManager.hpp"
+#include "RenderTarget.hpp"
+#include "SceneObject.hpp"
 #include "SceneTree.hpp"
+#include "WindowManager.hpp"
+#include "variant_utils.hpp"
 
 namespace KRE
 {
+	namespace
+	{
+		struct CameraScope
+		{
+			CameraScope(const CameraPtr& cam) 
+				: old_cam(nullptr)
+			{
+				if(cam) {
+					old_cam = DisplayDevice::getCurrent()->setDefaultCamera(cam);
+				}
+			}
+			~CameraScope()
+			{
+				if(old_cam) {
+					DisplayDevice::getCurrent()->setDefaultCamera(old_cam);
+				}
+			}
+			CameraPtr old_cam;
+		};
+
+		struct SceneTreeImpl : public SceneTree
+		{
+			SceneTreeImpl(const SceneTreePtr& parent) : SceneTree(parent) {}
+		};
+	}
+
+	SceneTree::SceneTree(const SceneTreePtr& parent)
+		: parent_(parent),
+		  children_(),
+		  objects_(),
+		  scopeable_(),
+		  camera_(nullptr),
+		  render_targets_(),
+		  render_target_window_(),
+		  clip_shape_(nullptr),
+		  position_(0.0f),
+		  rotation_(1.0f, 0.0f, 0.0f, 0.0f),
+		  scale_(1.0f),
+		  model_changed_(true),
+		  model_matrix_(1.0f),
+		  color_(nullptr)
+	{
+	}
+
+	SceneTreePtr SceneTree::create(SceneTreePtr parent)
+	{
+		return std::make_shared<SceneTreeImpl>(parent);
+	}
+
+	void SceneTree::removeObject(const SceneObjectPtr& obj)
+	{
+		objects_.erase(std::remove_if(objects_.begin(), objects_.end(), [obj](const SceneObjectPtr& object) {
+			return object == obj;
+		}), objects_.end());
+	}
+
+	void SceneTree::setPosition(const glm::vec3& position) 
+	{
+		position_ = position;
+		model_changed_ = true;
+	}
+
+	void SceneTree::setPosition(float x, float y, float z) 
+	{
+		position_ = glm::vec3(x, y, z);
+		model_changed_ = true;
+	}
+
+	void SceneTree::setPosition(int x, int y, int z) 
+	{
+		position_ = glm::vec3(float(x), float(y), float(z));
+		model_changed_ = true;
+	}
+
+	void SceneTree::setRotation(float angle, const glm::vec3& axis) 
+	{
+		rotation_ = glm::angleAxis(angle, axis);
+		model_changed_ = true;
+	}
+
+	void SceneTree::setRotation(const glm::quat& rot) 
+	{
+		rotation_ = rot;
+		model_changed_ = true;
+	}
+
+	void SceneTree::setScale(float xs, float ys, float zs) 
+	{
+		scale_ = glm::vec3(xs, ys, zs);
+		model_changed_ = true;
+	}
+
+	void SceneTree::setScale(const glm::vec3& scale) 
+	{
+		scale_ = scale;
+		model_changed_ = true;
+	}
+
+	const glm::mat4& SceneTree::getModelMatrix() const 
+	{
+		if(model_changed_) {
+			model_matrix_ = glm::translate(get_identity_matrix(), position_) * glm::toMat4(rotation_) * glm::scale(get_identity_matrix(), scale_);
+		}
+		return model_matrix_;
+	}
+
+	void SceneTree::preRender(const WindowPtr& wnd)
+	{
+		for(auto& obj : objects_) {
+			obj->preRender(wnd);
+		}
+
+		for(auto& child : children_) {
+			child->preRender(wnd);
+		}
+	}
+
+	void SceneTree::render(const WindowPtr& wnd) const
+	{
+		//if(scopeable_.isBlendEnabled()) {
+		//}
+
+		CameraScope cs(camera_);
+		ClipShapeScope::Manager cssm(clip_shape_, nullptr);
+		// blend
+		// color
+		//BlendModeScope bms(scopeable_);
+		//BlendEquationScope bes(scopeable_);
+		ColorScope color_scope(color_);
+
+		//XXX set global model matrix here. getModelMatrix()
+
+		// render all the objects and children into a render target if one exists.
+		// which is why we introduce a new scope
+		{
+			RenderTarget::RenderScope(!render_targets_.empty() ? render_targets_.front() : nullptr, render_target_window_);
+
+			for(auto& obj : objects_) {
+				wnd->render(obj.get());
+			}
+
+			for(auto& child : children_) {
+				child->render(wnd);
+			}
+		}
+
+		if(render_targets_.size() > 1) {
+			for(auto it = render_targets_.cbegin() + 1; it != render_targets_.cend(); ) {
+				RenderTarget::RenderScope(*it, render_target_window_);
+				wnd->render((it-1)->get());
+			}
+		}
+
+		// Output the last render target
+		if(!render_targets_.empty()) {
+			wnd->render(render_targets_.back().get());
+		}
+	}
+
+	const glm::vec3& get_xaxis()
+	{
+		static glm::vec3 x_axis(1.0f, 0.0f, 0.0f);
+		return x_axis;
+	}
+
+	const glm::vec3& get_yaxis()
+	{
+		static glm::vec3 y_axis(0.0f, 1.0f, 0.0f);
+		return y_axis;
+	}
+
+	const glm::vec3& get_zaxis()
+	{
+		static glm::vec3 z_axis(0.0f, 0.0f, 1.0f);
+		return z_axis;
+	}
+
+	const glm::mat4& get_identity_matrix()
+	{
+		static glm::mat4 imatrix(1.0f);
+		return imatrix;
+	}
 }
--- src/kre/SceneTree.hpp
+++ src/kre/SceneTree.hpp
@@ -24,13 +24,80 @@
 #pragma once
 
 #include <memory>
+#include <vector>
+
+#include "DisplayDeviceFwd.hpp"
+#include "ScopeableValue.hpp"
+#include "SceneFwd.hpp"
+#include "WindowManagerFwd.hpp"
 
 namespace KRE
 {
-	class SceneTree : std::enable_shared_from_this<SceneTree>
+	class SceneTree;
+	typedef std::shared_ptr<SceneTree> SceneTreePtr;
+	typedef std::weak_ptr<SceneTree> WeakSceneTreePtr;
+
+	class SceneTree : public std::enable_shared_from_this<SceneTree>
 	{
 	public:
-		SceneTree();
+		virtual ~SceneTree() {}
+		SceneTreePtr getParent() const { return parent_.lock(); }
+
+		void addObject(const SceneObjectPtr& obj) { objects_.emplace_back(obj); }
+		void clearObjects() { objects_.clear(); }
+		void removeObject(const SceneObjectPtr& obj);
+		void addChild(const SceneTreePtr& child) { children_.emplace_back(child); }
+
+		void preRender(const WindowPtr& wnd);
+		void render(const WindowPtr& wnd) const;
+
+		void setPosition(const glm::vec3& position);
+		void setPosition(float x, float y, float z=0.0f);
+		void setPosition(int x, int y, int z=0);
+		const glm::vec3& getPosition() const { return position_; }
+
+		void setRotation(float angle, const glm::vec3& axis);
+		void setRotation(const glm::quat& rot);
+		const glm::quat& getRotation() const { return rotation_; }
+
+		void setScale(float xs, float ys, float zs=1.0f);
+		void setScale(const glm::vec3& scale);
+		const glm::vec3& getScale() const { return scale_; }
+
+		void clearRenderTargets() { render_targets_.clear(); }
+		void addRenderTarget(const RenderTargetPtr& render_target) { render_targets_.emplace_back(render_target); }
+		const std::vector<RenderTargetPtr>& getRenderTargets() const { return render_targets_; }
+
+		const glm::mat4& getModelMatrix() const;
+
+		static SceneTreePtr create(SceneTreePtr parent);
+	protected:
+		explicit SceneTree(const SceneTreePtr& parent);
 	private:
+
+		WeakSceneTreePtr parent_;
+		std::vector<SceneTreePtr> children_;
+		std::vector<SceneObjectPtr> objects_;
+
+		ScopeableValue scopeable_;
+		CameraPtr camera_;
+		std::vector<RenderTargetPtr> render_targets_;
+		rect render_target_window_;
+		
+		RenderablePtr clip_shape_;
+
+		glm::vec3 position_;
+		glm::quat rotation_;
+		glm::vec3 scale_;
+
+		bool model_changed_;
+		mutable glm::mat4 model_matrix_;
+
+		ColorPtr color_;
 	};
+
+	const glm::vec3& get_xaxis();
+	const glm::vec3& get_yaxis();
+	const glm::vec3& get_zaxis();
+	const glm::mat4& get_identity_matrix();
 }
