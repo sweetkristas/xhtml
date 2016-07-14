0f0ca9a59bde26f3742484ed2ac0619d7db81b32
Kristina Simpson <sweet.kristas@gmail.com>
2016-07-14 18:01:36 +1200

    Added missing files from last commit.

---
+++ src/xhtml/text_edit.cpp
@@ -0,0 +1,91 @@
+/*
+	Copyright (C) 2016 by Kristina Simpson <sweet.kristas@gmail.com>
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+	1. The origin of this software must not be misrepresented; you must not
+	   claim that you wrote the original software. If you use this software
+	   in a product, an acknowledgment in the product documentation would be
+	   appreciated but is not required.
+
+	2. Altered source versions must be plainly marked as such, and must not be
+	   misrepresented as being the original software.
+
+	3. This notice may not be removed or altered from any source
+	   distribution.
+*/
+
+#include "text_edit.hpp"
+
+namespace controls
+{
+	using namespace KRE;
+
+	TextEdit::TextEdit(const rect& area, TextEditType type, const std::string& default_value)
+		: SceneObject("TextEdit"),
+		  type_(type),
+		  current_line_text_(default_value),
+		  multi_line_text_(),
+		  loc_(area),
+		  background_color_(Color::colorWhite()),
+		  text_color_(new Color(Color::colorBlack())),
+		  renderable_(nullptr)
+	{
+	}
+
+	TextEditPtr TextEdit::create(const rect& area, TextEditType type, const std::string& default_value)
+	{
+		return std::make_shared<TextEdit>(area, type, default_value);
+	}
+
+	void TextEdit::setHandlers(change_handler onchange)
+	{
+		on_change_ = onchange;
+	}
+
+	void TextEdit::preRender(const WindowPtr& wm)
+	{
+	}
+
+	bool TextEdit::handleMouseMotion(bool claimed, const point& p, unsigned keymod) 
+	{
+		return claimed;
+	}
+
+	bool TextEdit::handleMouseButtonUp(bool claimed, const point& p, unsigned buttons, unsigned keymod) 
+	{
+		return claimed;
+	}
+
+	bool TextEdit::handleMouseButtonDown(bool claimed, const point& p, unsigned buttons, unsigned keymod) 
+	{
+		return claimed;
+	}
+
+	bool TextEdit::handleMouseWheel(bool claimed, const point& p, const point& delta, int direction) 
+	{
+		return claimed;
+	}
+
+	bool TextEdit::handleKeyDown(bool claimed, const SDL_Keysym& keysym, bool repeat, bool pressed) 
+	{
+		LOG_INFO("key down: " << keysym.sym << "; repeat: " << (repeat ? "true" : "false") << "; " << (pressed ? "pressed" : "released"));
+		return claimed;
+	}
+
+	bool TextEdit::handleTextInput(bool claimed, const std::string& text) 
+	{
+		return claimed;
+	}
+
+	bool TextEdit::handleTextEditing(bool claimed, const std::string& text, int start, int length) 
+	{
+		return claimed;
+	}
+}
---
+++ src/xhtml/text_edit.hpp
@@ -0,0 +1,86 @@
+/*
+	Copyright (C) 2016 by Kristina Simpson <sweet.kristas@gmail.com>
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+	1. The origin of this software must not be misrepresented; you must not
+	   claim that you wrote the original software. If you use this software
+	   in a product, an acknowledgment in the product documentation would be
+	   appreciated but is not required.
+
+	2. Altered source versions must be plainly marked as such, and must not be
+	   misrepresented as being the original software.
+
+	3. This notice may not be removed or altered from any source
+	   distribution.
+*/
+
+#pragma once
+
+#include <functional>
+
+#include "AttributeSet.hpp"
+#include "geometry.hpp"
+#include "SceneObject.hpp"
+#include "WindowManagerFwd.hpp"
+#include "event_listener.hpp"
+#include "FontDriver.hpp"
+#include "scrollable.hpp"
+
+namespace controls
+{
+	class TextEdit;
+	typedef std::shared_ptr<TextEdit> TextEditPtr;
+
+	typedef std::function<void(const std::string& s)> change_handler;
+
+	enum class TextEditType {
+		SINGLE_LINE,
+		MULTI_LINE,
+	};
+
+	class TextEdit : public KRE::SceneObject, public EventListener
+	{
+	public:
+		TextEdit(const rect& area, TextEditType type=TextEditType::SINGLE_LINE, const std::string& default_value=std::string());
+		~TextEdit() {}
+
+		void setHandlers(change_handler onchange);
+
+		const KRE::FontRenderablePtr& getRenderable() const { return renderable_; }
+
+		void preRender(const KRE::WindowPtr& wm) override;
+
+		static TextEditPtr create(const rect& area, TextEditType type=TextEditType::SINGLE_LINE, const std::string& default_value=std::string());
+	private:
+		bool handleKeyDown(bool claimed, const SDL_Keysym& keysym, bool repeat, bool pressed);
+		bool handleKeyUp(bool claimed, const SDL_Keysym& keysym, bool repeat, bool pressed) { return claimed; }
+		bool handleTextInput(bool claimed, const std::string& text);
+		bool handleTextEditing(bool claimed, const std::string& text, int start, int length);
+
+		bool handleMouseMotion(bool claimed, const point& p, unsigned keymod) override;
+		bool handleMouseButtonUp(bool claimed, const point& p, unsigned buttons, unsigned keymod) override;
+		bool handleMouseButtonDown(bool claimed, const point& p, unsigned buttons, unsigned keymod) override;	
+		bool handleMouseWheel(bool claimed, const point& p, const point& delta, int direction) override;
+
+		TextEditType type_;
+		std::string current_line_text_;
+		std::vector<std::string> multi_line_text_;
+		rect loc_;
+		
+		change_handler on_change_;
+		
+		KRE::Color background_color_;
+		KRE::ColorPtr text_color_;
+
+		KRE::FontRenderablePtr renderable_;
+
+		scrollable::ScrollbarPtr scollbar_;
+	};
+}
